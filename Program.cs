401302a585d707cc35401e59ccb9a966a40eb16f
Alan Mervitz <amervitz@users.noreply.github.com>
2021-03-23 11:05:40 -0600

    Show output as comments

--- Program.cs
+++ Program.cs
@@ -9,9 +9,11 @@ namespace Guid5
         {
             var namespaceId = GuidUtility.Create(GuidUtility.DnsNamespace, "alanmervitz.com");
             Console.WriteLine($"namespaceId: {namespaceId}");
-
+            // namespaceId: 3590eed8-4dbf-5b5c-b9a5-7cc144bb7282
+            
             var name = GuidUtility.Create(namespaceId, "test");
             Console.WriteLine($"name: {name}");
+            // name: 1c5b0d5a-c333-5e9a-9ec3-646da22795ca
         }
     }
 }
