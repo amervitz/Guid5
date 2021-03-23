98ff68fe21e21e6ed860c2e6254738d9fad75f78
Alan Mervitz <amervitz@users.noreply.github.com>
2021-03-23 10:48:39 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Faithlife.Utility;
+using System;
+
+namespace Guid5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var namespaceId = GuidUtility.Create(GuidUtility.DnsNamespace, "alanmervitz.com");
+            Console.WriteLine($"namespaceId: {namespaceId}");
+
+            var name = GuidUtility.Create(namespaceId, "test");
+            Console.WriteLine($"name: {name}");
+        }
+    }
+}
